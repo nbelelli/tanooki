2b37352fbe55ed107511f020e0257765f4150289
Nicolas Belelli <nbelelli@gmail.com>
2021-08-14 20:24:16 -0300

    Header and nav links

---
+++ src/components/Header/Header.js
@@ -0,0 +1,13 @@
+import NavBar from '../NavBar/NavBar';
+import './../Header/Header.css';
+
+function Header() {
+	return (
+		<header className="header">
+			<div class="title">Tanooki Games</div>
+			<NavBar />
+		</header>
+	);
+}
+
+export default Header;
