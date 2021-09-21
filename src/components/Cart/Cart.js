81f6ca14cc39246a56e4a575bc8ea596b686b328
Nicolas Belelli <nbelelli@gmail.com>
2021-09-21 09:54:02 -0300

    cartView2

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,5 +1,33 @@
+import { useContext, useState } from 'react';
+import { Link } from 'react-router-dom';
+import { context } from '../../CartContext/CartContext';
+
 const Cart = () => {
-	return <div>CART.JS</div>;
+	const { cart, removeProduct } = useContext(context);
+	console.log(cart);
+
+	if (cart.length === 0) {
+		return (
+			<>
+				<div>no hay productos</div>
+				<Link to="/">
+					<button>Volver al Home</button>
+				</Link>
+			</>
+		);
+	}
+	return (
+		<div>
+			{cart.map((prod) => (
+				<div>
+					<div>
+						{prod.title} x {prod.quantity}
+					</div>
+					<button onClick={removeProduct(prod.id)}>eliminar</button>
+				</div>
+			))}
+		</div>
+	);
 };
 
 export default Cart;
